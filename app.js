8cd7a1346b0359e64e88995b719d186d86431f5f
Corey Lambert <157275713+CsTL0522@users.noreply.github.com>
2025-02-09 02:25:29 -0600

    created api route to get songs

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // set up.. Similiar to default tags for html
 const express = require("express")
+var cors = require('cors') // use this to use both
 // activate to tell to be an express variable
 const app = express()
+app.use(cors())
 const router = express.Router()
 
+
 // start web server... app.listen(portnumber,function)
-app.listen(3000,function() {
-    console.log("listening on port 3000")
-})
+// app.listen(3000,function() {
+//     console.log("listening on port 3000")
+// })
 
 // making an api using routes
 // routes are used to handle browser requests, 
@@ -16,9 +19,29 @@ app.listen(3000,function() {
 
 // GET requests for http://localhost:3000/hello
 
-app.get("/hello", function(req,res) {
-    res.send("<h1>Hello Express</h1>")
+router.get("/songs", function(req,res) {
+    const songs = [
+        {
+            title: "We Found Love",
+            artist: "Rihanna",
+            popularity: 10,
+            releaseDate: new Date(2011, 9, 22),
+            genre: ["electro house"]
+        },
+        {
+            title: "Happy",
+            artist: "Pharrell Williams",
+            popularity: 10,
+            releaseDate: new Date(2013, 11, 21),
+            genre: ["soul", "new soul"]
+        },
+
+    ]
+
+    res.json(songs)
 })
-app.get("/goodbye", function(req,res) {
-    res.send("<h1>Goodbye Express</h1>");
-});
+
+// all requests taht use an api start with /api...
+    // localhost:3000/api/songs
+app.use("/api", router)
+app.listen(3000)
