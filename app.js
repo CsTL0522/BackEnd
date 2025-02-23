f5c3330d13ed45d7757378856314f1e8ebf676d4
Corey Lambert <157275713+CsTL0522@users.noreply.github.com>
2025-02-23 06:07:51 -0600

    tutorial 2 steps

--- app.js
+++ app.js
@@ -37,6 +37,32 @@ router.post("/songs", async(req,res) =>{
     }
 })
 
+// grab single song
+
+router.get("/songs/:id", async (req,res) =>{
+    try{
+        const song = await Song.findById(req.params.id)
+        res.json(song)
+    }
+    catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+
+//update uses put
+router.put("/songs/:id", async (req,res) =>{
+    try{
+        const song = req.body
+        await Song.updateOne({_id: req.params.id},song)
+        console.log(song)
+        res.sendStatus(204)
+    }
+    catch(err){
+       res.status(400).send(err)
+    }
+})
+
 
 app.use("/api", router)
 
